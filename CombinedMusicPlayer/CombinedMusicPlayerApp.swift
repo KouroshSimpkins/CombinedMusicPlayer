13c28496e2307a0e53f8f70584b314fcb2ee6a52
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2023-05-22 00:46:48 +0100

    Initial Commit

---
+++ CombinedMusicPlayer/CombinedMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinedMusicPlayerApp.swift
+//  CombinedMusicPlayer
+//
+//  Created by Kourosh Simpkins on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CombinedMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

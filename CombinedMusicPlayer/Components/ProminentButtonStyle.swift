0b6d83649e97ddf3d2dfcb379ee6e3194367f9c9
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2023-06-01 18:44:51 +0100

    New Button Style and Search View

---
+++ CombinedMusicPlayer/Components/ProminentButtonStyle.swift
@@ -0,0 +1,32 @@
+//
+// Created by Kourosh Simpkins on 01/06/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProminentButtonStyle: ButtonStyle {
+
+	@Environment(\.colorScheme) private var colorScheme: ColorScheme
+
+	func makeBody(configuration: Self.Configuration) -> some View {
+		configuration.label
+				.font(.title3.bold())
+				.foregroundColor(.accentColor)
+				.padding()
+				.background(backgroundColour.cornerRadius(8))
+				.scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+				.animation(.easeInOut(duration: 0.2))
+	}
+
+	private var backgroundColour: Color {
+		Color(uiColor: (colorScheme == .dark ? .secondarySystemBackground : .systemBackground))
+	}
+}
+
+// MARK: - Button Style Extension
+extension ButtonStyle where Self == ProminentButtonStyle {
+	static var prominent: ProminentButtonStyle {
+		ProminentButtonStyle()
+	}
+}
---
+++ CombinedMusicPlayer/Screens/NewSearchView.swift
@@ -0,0 +1,5 @@
+//
+// Created by Kourosh Simpkins on 01/06/2023.
+//
+
+import Foundation
